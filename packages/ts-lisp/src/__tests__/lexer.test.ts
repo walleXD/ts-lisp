b4eac5b7a6a4ea2b3b906cbc93d81ff7efb5da85
Walee Ahmed <dev@walee.me>
2019-07-18 23:49:23 -0400

    added tokenization to lexer

--- packages/ts-lisp/src/__tests__/lexer.test.ts
+++ packages/ts-lisp/src/__tests__/lexer.test.ts
@@ -1,25 +1,111 @@
-import { generateRawTokens } from '../lexer'
+import {
+  generateRawTokens,
+  generateTokens,
+  tokenize,
+  TokenTypes,
+  Token
+} from '../lexer'
 
-describe('generate raw tokens', (): void => {
-  it('handle input', (): void => {
-    const basicProgram = `(ADD 1 2)`
+describe('lexer', (): void => {
+  describe('raw tokens', (): void => {
+    it('handle input', (): void => {
+      const basicProgram = `(ADD 1 2)`
 
-    expect(generateRawTokens(basicProgram)).toEqual([
-      '(',
-      'ADD',
-      '1',
-      '2',
-      ')'
-    ])
+      expect(generateRawTokens(basicProgram)).toEqual([
+        '(',
+        'ADD',
+        '1',
+        '2',
+        ')'
+      ])
+    })
+
+    it('handle strings', (): void => {
+      const basicProgram = `("hello world")`
+
+      expect(generateRawTokens(basicProgram)).toEqual([
+        '(',
+        '"hello world"',
+        ')'
+      ])
+    })
   })
 
-  it('handle strings', (): void => {
-    const basicProgram = `("hello world")`
+  describe('tokenization', (): void => {
+    it('numbers', (): void => {
+      expect(tokenize('222')).toEqual({
+        type: TokenTypes.NUMERIC_ATOM,
+        value: 222,
+        size: 3
+      })
+    })
 
-    expect(generateRawTokens(basicProgram)).toEqual([
-      '(',
-      'hello world',
-      ')'
-    ])
+    it('open paren', (): void => {
+      expect(tokenize('(')).toEqual({
+        type: TokenTypes.PAREN_OPEN,
+        value: '(',
+        size: 1
+      })
+    })
+
+    it('close paren', (): void => {
+      expect(tokenize(')')).toEqual({
+        type: TokenTypes.PAREN_CLOSE,
+        value: ')',
+        size: 1
+      })
+    })
+
+    it('strings', (): void => {
+      expect(tokenize('"Hello World"')).toEqual({
+        type: TokenTypes.STRING_ATOM,
+        value: 'Hello World',
+        size: 11
+      })
+    })
+
+    it('literals', (): void => {
+      expect(tokenize('ADD')).toEqual({
+        type: TokenTypes.LITERAL_ATOM,
+        value: 'ADD',
+        size: 3
+      })
+    })
+
+    it('generate tokens', (): void => {
+      const program = '(ADD 1 2)'
+      const tokens: Token[] = [
+        {
+          type: TokenTypes.PAREN_OPEN,
+          value: '(',
+          size: 1
+        },
+        {
+          type: TokenTypes.LITERAL_ATOM,
+          value: 'ADD',
+          size: 3
+        },
+        {
+          type: TokenTypes.NUMERIC_ATOM,
+          value: 1,
+          size: 1
+        },
+        {
+          type: TokenTypes.NUMERIC_ATOM,
+          value: 2,
+          size: 1
+        },
+        {
+          type: TokenTypes.PAREN_CLOSE,
+          value: ')',
+          size: 1
+        }
+      ]
+
+      const generatedTokens = generateRawTokens(program)
+      expect(generateTokens(generatedTokens)).toEqual(
+        tokens
+      )
+    })
   })
 })
--- packages/ts-lisp/src/lexer.ts
+++ packages/ts-lisp/src/lexer.ts
@@ -1,3 +1,8 @@
+/**
+ * Takes a lisp program as string and returns array of raw token strings
+ * @param {string} program
+ * @returns {string[]} array of token strings
+ */
 export const generateRawTokens = (
   program: string
 ): string[] =>
@@ -6,10 +11,105 @@ export const generateRawTokens = (
     .map((val, i): string =>
       i % 2 == 0
         ? val.replace(/\(/g, ' ( ').replace(/\)/g, ' ) ')
-        : val.replace(/\s/g, '!whitespace!')
+        : `"${val.replace(/\s/g, '!whitespace!')}"`
     )
     .join(' ')
     .trim()
     .split(' ')
     .map((val): string => val.replace(/!whitespace!/, ' '))
     .filter((val): boolean => val != '')
+
+/**
+ * Types of tokens
+ * @enum TokenTypes
+ */
+export enum TokenTypes {
+  PAREN_OPEN,
+  PAREN_CLOSE,
+  NUMERIC_ATOM,
+  LITERAL_ATOM,
+  STRING_ATOM,
+  ERROR
+}
+
+/**
+ * Interface for tokens
+ * @interface Token
+ */
+export interface Token {
+  type: TokenTypes
+  value: string | number
+  size: number
+}
+
+/**
+ * Generates token for numeric atoms
+ * @param rawToken
+ * @returns {Token} token for numeric atom
+ */
+const tokenizeNum = (rawToken: string): Token => ({
+  size: rawToken.length,
+  value: Number(rawToken),
+  type: TokenTypes.NUMERIC_ATOM
+})
+
+/**
+ * Generates token for parenthesis atoms
+ * @param rawToken Raw string token
+ * @returns {Token} token for literal atom
+ */
+const tokenizeParen = (rawToken: string): Token => ({
+  size: 1,
+  value: rawToken,
+  type:
+    rawToken === '('
+      ? TokenTypes.PAREN_OPEN
+      : TokenTypes.PAREN_CLOSE
+})
+
+/**
+ * Generates token for string atoms
+ * @param rawToken Raw string token
+ * @returns {Token} token for string atom
+ */
+const tokenizeString = (rawToken: string): Token => ({
+  size: rawToken.length,
+  value: rawToken,
+  type: TokenTypes.STRING_ATOM
+})
+
+/**
+ * Generates token for literal atoms
+ * @param rawToken Raw string token
+ * @returns {Token} token for literal atom
+ */
+const tokenizeLiteral = (rawToken: string): Token => ({
+  size: rawToken.length,
+  value: rawToken,
+  type: TokenTypes.LITERAL_ATOM
+})
+
+/**
+ * Takes a string token and then return appropriate token
+ * @param {string} rawToken Raw string token
+ * @returns {Token} Appropriate token based the given raw token
+ */
+export const tokenize = (rawToken: string): Token => {
+  if (Number(rawToken)) return tokenizeNum(rawToken)
+  else if (rawToken === '(' || rawToken === ')')
+    return tokenizeParen(rawToken)
+  else if (rawToken.charAt(0) === '"')
+    return tokenizeString(
+      rawToken.substring(1, rawToken.length - 1)
+    )
+  else return tokenizeLiteral(rawToken)
+}
+
+/**
+ * Generates an array of Tokens
+ * @param {string[]} rawTokens Array of raw tokens
+ * @returns {Token[]} Array of Token
+ */
+export const generateTokens = (
+  rawTokens: string[]
+): Token[] => rawTokens.map(tokenize)
